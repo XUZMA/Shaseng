7bccdd9580fad0846c054e85a492574fb447709a
XUZMA <xuzmnsccsz@yeah.net>
2014-12-19 23:29:08 +0800

    bash example from tutorialspoint.com/unix/unix-shell.htm

---
+++ bash/bs001.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# examples from
+# http://www.tutorialspoint.com/unix/unix-shell.htm
+
+#!/bin/bash
+
+# $chmod +x test.sh
+# $./test.sh
+# Author : XZM
+#
+pwd
+ls
+
+echo "What is your name?"
+read PERSON
+echo "Hello, $PERSON"

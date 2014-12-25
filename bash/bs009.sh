7e952edb0743eeabf9615930ad2a7290d44f6302
XUZMA <xuzmnsccsz@yeah.net>
2014-12-25 22:20:34 +0800

    bash exmaple from bash.cyberciti.biz/guide/Chapter_2_Challenges

---
+++ bash/bs009.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# examples from
+# http://bash.cyberciti.biz/guide/Chapter_2_Challenges
+# Script to print currently logged in users information, and current date & time.
+clear
+echo "Hello $USER"
+echo -e "Today is \c ";date
+echo -e "Number of user login : \c" ; who | wc -l
+echo "Calendar"
+cal
+exit 0

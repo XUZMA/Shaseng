1922242d31a73fbaa46b176ca92a64a21db96f54
XUZMA <xuzmnsccsz@yeah.net>
2015-01-02 14:56:57 +0800

    bash examples from bash.cyberciti.biz/guide/Quoting

--- bash/bs016.sh
+++ bash/bs016.sh
@@ -2,8 +2,9 @@
 
 #
 # examples from
-# http://bash.cyberciti.biz/guide/Quoting
+# http://bash.cyberciti.biz/guide/Export_Variables
 # Export Variables
+#
 # The export builtin automatically exports to the environment of child processes.
 
 export backup="/nas10/mysql"

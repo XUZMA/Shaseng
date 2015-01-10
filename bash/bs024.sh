a109627ea83fd725e482238b32b9e58b13e2d7fb
XUZMA <xuzmnsccsz@yeah.net>
2015-01-10 23:09:14 +0800

    bash examples from bash.cyberciti.biz/guide/Recalling_command_history

---
+++ bash/bs024.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## http://bash.cyberciti.biz/guide/Recalling_command_history
+## Recalling command history
+## 
+## Bash keeps a command history in buffer or a default file called ~/.bash_history.
+## The history buffer can hold many commands.
+## Use history command to display a list of command you entered at a shell prompt. You can also repeat commands stored in history.
+## The history command displays the history list with line numbers.
+## By default history is enabled but can be disabled using set builtin command.
+## You can recall the basic command with arrow keys.
+## 
+## Recall commands
+## Simply hit [Up] and [Down] arrow keys.
+## 
+## To repeat last command
+## Just type !! at a shell prompt:
+## 
+## To repeat last command started with ...
+## Recall the most recent command starting with vn
+## 
+## To repeat a command by its number
+## Recall to command line number 13:
+## 
+
+date
+!!

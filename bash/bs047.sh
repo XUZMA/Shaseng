da5ffb4eb140320ff3e6bd6cf9367fde2e8f68f2
XUZMA <xuzmnsccsz@yeah.net>
2015-02-02 22:26:50 +0800

    bash example from bash.cyberciti.biz/guide/Shell_command_line_parameters

---
+++ bash/bs047.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#    http://bash.cyberciti.biz/guide/Shell_command_line_parameters
+#    
+#    Shell command line parameters
+#    
+#    Most Linux command can take different actions depending on the command line arguments supplied to the command.
+#    
+#    Command line parameters different names
+#    A Command line parameter also known as:
+#        - Command line options
+#        - Options
+#        - Positional parameters
+#        - Flag
+#        - Switches or switch
+#        - Command-line arguments
+#    
+#    Why use command line arguments
+#        - Telling the command/utility which option to use.
+#        - Informing the utility/command which file or group of files to process (reading/writing of files).
+
+date +"%d-%m-%Y"

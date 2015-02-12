8b69af76a1ee1a788ebf65beb4875811a9589cf5
XUZMA <xuzmnsccsz@yeah.net>
2015-02-12 21:50:11 +0800

    the names of host, database, user, password for login

---
+++ PHP/lpmj-query-login-ch10/login.php
@@ -0,0 +1,13 @@
+<?php
+
+    // $hn, host name tell PHP which computer to use when connecting to a database.
+    $hn = 'localhost';
+
+    // $db, database name.
+    $db = 'publications';
+
+    // $un, user name.
+    $un = 'USER';
+    // $pw, password.
+    $pw = 'PASSWORD';
+?>

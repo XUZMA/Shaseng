714418164bb73817a6aa67742c655b6a9ad5e10f
XUZMA <xuzmnsccsz@yeah.net>
2015-02-19 21:14:21 +0800

    A Simple PHP form handler

--- PHP/lpmj-ch11/formtest.php
+++ PHP/lpmj-ch11/formtest.php
@@ -1,10 +1,17 @@
 <?php // formtest.php
+
+    if (isset($_POST['name']))
+        $name = $_POST['name'];
+    else
+        $name = "(Not entered)";
+
     echo <<<_END
         <html>
             <head>
                 <title>Form Test</title>
             </head>
             <body>
+                Your name is: $name<br>
                 <form method="post" action="formtest.php">
                     What is your name?
                     <input type="text" name="name">

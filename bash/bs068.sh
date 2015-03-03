9e15319af3d2cb7c446a58e5e5acc4824d0c5f67
XUZMA <xuzmnsccsz@yeah.net>
2015-03-03 23:13:49 +0800

    bash example from bash.cyberciti.biz/guide/Command_substitution

---
+++ bash/bs068.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#    http://bash.cyberciti.biz/guide/Command_substitution
+#    Command substitution
+#    
+#    Syntax
+#    You can use the grave accent (`) to perform a command substitution. The syntax is:
+#        `command-name`
+#    OR
+#        $(command-name)
+#    
+
+echo "Today is $(date)"

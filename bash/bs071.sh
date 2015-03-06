314e20a58818501b2bacf32cb17a0c1b29e33cab
XUZMA <xuzmnsccsz@yeah.net>
2015-03-06 22:47:36 +0800

    bash example from bash.cyberciti.biz/guide/Standard_output

---
+++ bash/bs071.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#    http://bash.cyberciti.biz/guide/Standard_output
+#    Standard output
+#    
+#    Standard output is used by a command to writes (display) its output.
+#    The default is the screen.
+#    It is denoted by one number (1).
+#    Also known as stdout.
+#    The default standard output is the screen.
+#    > is output redirection symbol and syntax is:
+#    command > output.file.name
+
+ls > output.txt

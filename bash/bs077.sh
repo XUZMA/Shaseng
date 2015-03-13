89000d0cfcf25f7c343dad0164d5a2b333be49ec
XUZMA <xuzmnsccsz@yeah.net>
2015-03-13 23:37:08 +0800

    bash.cyberciti.biz/guide/Redirection_of_standard_error

---
+++ bash/bs077.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#    http://bash.cyberciti.biz/guide/Redirection_of_standard_error
+#    Redirection of standard error
+#    
+#    To redirect standard error into file called error.log, enter:
+#    command-name 2>error.log
+
+find /home -name .profile 2>error

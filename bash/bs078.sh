147cb850b3917eae56e3240e2e029d7dca587306
XUZMA <xuzmnsccsz@yeah.net>
2015-03-14 23:19:55 +0800

    bash examples from bash.cyberciti.biz/guide/Redirection_of_standard_output

---
+++ bash/bs078.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#    http://bash.cyberciti.biz/guide/Redirection_of_standard_output
+#    Redirection of standard output
+#    To redirect output simple use the following syntax:
+#    command > /path/to/file
+
+echo "Today is $(date)" 1> ./tmp.txt

27c4ba73b2dde6207fa7f320a1acf83a0efdc2b7
XUZMA <xuzmnsccsz@yeah.net>
2015-03-15 22:49:55 +0800

    bash example from bash.cyberciti.biz/guide/Appending_redirected_output

---
+++ bash/bs079.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#    http://bash.cyberciti.biz/guide/Appending_redirected_output
+#    Appending redirected output
+#    You can append the output to the same file using >> operator.
+
+echo "Today is $(date)" 1>>./now.txt

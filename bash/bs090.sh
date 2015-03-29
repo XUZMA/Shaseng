adc6c665e9891f066d3d5ec09a6985be05140e72
XUZMA <xuzmnsccsz@yeah.net>
2015-03-29 22:44:30 +0800

    bash.cyberciti.biz/guide/Linking_Commands

---
+++ bash/bs090.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#    http://bash.cyberciti.biz/guide/Linking_Commands
+
+#    Operator
+#            ;
+#    
+#    Syntax
+#            command1; command2
+#    
+#    Description
+#            Separates commands that are executed in sequence.
+#    
+#    Example
+#            In this example, pwd is executed only after date command completes.
+
+date ; pwd

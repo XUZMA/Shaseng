e71cc1eea19d8c8d9b5bbb6553be98e206545e32
XUZMA <xuzmnsccsz@yeah.net>
2015-04-02 23:20:48 +0800

    bash.cyberciti.biz/guide/Linking_Commands

---
+++ bash/bs094.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#    http://bash.cyberciti.biz/guide/Linking_Commands
+
+#    Operator
+#            |
+#    
+#    Syntax
+#            command1 | command2
+#    
+#    Description
+#            Linux shell pipes join the standard output of command1 to the standard input of command2. 
+#    
+#    Example
+
+ps aux | grep httpd
